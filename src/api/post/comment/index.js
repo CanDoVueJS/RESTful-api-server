4f6e0802d22334eed8567f6bac7a3212cc60fcfe
Evan Moon <bboydart91@gmail.com>
2019-02-09 15:36:16 +0900

    하드코딩된 Comment 생성 로직을 모델 메소드로 변경

--- src/api/post/comment/index.js
+++ src/api/post/comment/index.js
@@ -6,17 +6,13 @@ const router = Router({ mergeParams: true });
 
 router.post('/comments', isAuthenticated(), async (req, res) => {
   try {
-    const comment = await Comment.create({
+    await req.post.createComment({
       UserId: req.user.id,
-      PostId: req.post.id,
       contents: req.body.contents,
     });
-    console.log(comment);
     res.status(201).json({});
   }
   catch (e) {
-    console.error(e);
-    res.status(500).json(e);
   }
 });
 
