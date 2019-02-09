e38e78ef1efa28414b605904d542db60beb88fe1
Evan Moon <bboydart91@gmail.com>
2019-02-09 15:36:54 +0900

    Comment의 POST , DELETE 메소드에 대해서 400, 404 예외처리 추가

--- src/api/post/comment/index.js
+++ src/api/post/comment/index.js
@@ -13,6 +13,12 @@ router.post('/comments', isAuthenticated(), async (req, res) => {
     res.status(201).json({});
   }
   catch (e) {
+    if (e.name === 'SequelizeValidationError') {
+      res.status(400).json({ msg: '잘못된 요청 입니다' });
+    }
+    else {
+      res.status(500).json(e);
+    }
   }
 });
 
@@ -20,8 +26,15 @@ router.delete('/comments/:commentId', isAuthenticated(), async (req, res) =>{
   const id = req.params.commentId;
   const user = req.user;
   const comment = await Comment.findByPk(id);
-  if (!comment.isMyComment(user)) {
-    res.status(403).json().json({ msg: '자신의 댓글만 삭제하실 수 있습니다.' });
+
+  if (!comment) {
+    res.status(404).json({ msg: '존재하지 않는 댓글입니다.' });
+  }
+  else if (!req.post.hasComment(id)) {
+    res.status(404).json({ msg: '이 게시물에 존재하지 않는 댓글입니다.' });
+  }
+  else if (!comment.isMyComment(user)) {
+    res.status(403).json({ msg: '자신의 댓글만 삭제하실 수 있습니다.' });
   }
 
   try {
