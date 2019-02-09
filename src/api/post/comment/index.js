863c51158a96fed05b865b90a82bc54a71d5fc60
Evan Moon <bboydart91@gmail.com>
2019-02-09 15:37:09 +0900

    Comment의 PUT 메소드 추가

--- src/api/post/comment/index.js
+++ src/api/post/comment/index.js
@@ -46,4 +46,34 @@ router.delete('/comments/:commentId', isAuthenticated(), async (req, res) =>{
   }
 });
 
+router.put('/comments/:commentId', isAuthenticated(), async (req, res) => {
+  const id = req.params.commentId;
+  const user = req.user;
+  const comment = await Comment.findByPk(id);
+
+  if (!comment) {
+    res.status(404).json({ msg: '존재하지 않는 댓글입니다.' });
+  }
+  else if (!req.post.hasComment(id)) {
+    res.status(404).json({ msg: '이 게시물에 존재하지 않는 댓글입니다.' });
+  }
+  else if (!comment.isMyComment(user)) {
+    res.status(403).json({ msg: '자신의 댓글만 수정하실 수 있습니다.' });
+  }
+
+  try {
+    comment.contents = req.body.contents;
+    await comment.save();
+    res.status(200).json({});
+  }
+  catch (e) {
+    if (e.name === 'SequelizeValidationError') {
+      res.status(400).json({ msg: '잘못된 요청 입니다' });
+    }
+    else {
+      res.status(500).json(e);
+    }
+  }
+});
+
 export default router;
